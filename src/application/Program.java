c5a19abd8cfe46528847a98c3d189e31fb0111dc
Moisés Ribeiro <moises.rsjr@gmail.com>
2019-05-23 14:24:41 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Iniciando o projeto em Java 23/05/2019!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
